297139343b6120d6de2686a1f65f395c45c94f97
sanjiv <sanjiv.chaudhary@yipl.com.np>
2022-07-24 17:54:44 +0545

    final setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <>
+      <div className="text-primary text-[50px]"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        box: "0px 4px 40px rgba(0, 0, 0, 0.25)",
+        "show-more-info": "0px 8px 24px rgba(0, 50, 76, 0.15)",
+      },
+      screens: {
+        print: { raw: "print" },
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        primary: "#043A27",
+        black: "#001E2D",
+        white: "#ffffff",
+      },
+    },
+    plugins: [],
+  },
+};
